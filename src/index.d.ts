2ee8d03f428880e736b7782899218cd698edd7df
Dong Nguyen <ntdong98.it@gmail.com>
2021-09-24 14:14:55 +0700

    Clean up sample files

--- src/index.d.ts
+++
@@ -1,4 +0,0 @@
-export { default as FormBuilder } from "./containers/FormBuilder";
-export { default as FormGenerator } from "./containers/FormGenerator";
-export type { FormControlType, ControlType } from "./shared/types/FormControlType";
-export { parseTemplate } from "./shared/utils/parseTemplate";
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,5 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import React from "react";
-import ReactDOM from "react-dom";
-import Sample from "./samples/Sample";
 import "./index.css";
-
-ReactDOM.render(
-  <React.StrictMode>
-    <Sample />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
 
 export { default as FormBuilder } from "./containers/FormBuilder";
 export { default as FormGenerator } from "./containers/FormGenerator";
